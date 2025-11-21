36cb097f61f3993710fa076cd8234f2cf9b49b28
Wilizilla <wilian.ventura@gmail.com>
2025-11-21 00:55:50 -0300

    atualização servidor

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require ('express');
-const app = express();
+// EXPRESS serve para trabalhar nas rotas do do site
+
+const express = require ('express'); // importação de módulo da pasta node-modules
+const app = express(); // por convenção o nome é app, mas poderia ser outro nome
 const port = 3303; // a porta normal seria 3000
 
+
+// função para o servidor escutar alguma porta
 app.listen (3303, (error) => {
     if(error) {
         console.log("Erro: servidor offline")
         return;
     }
     console.log("Sucesso: servidor online");
-})
+});
+
+// função para get na raiz do site (requisição e resposta)
+app.get('/', (req, res) => {
+    res.send ('Olá, mundo!');
+});
