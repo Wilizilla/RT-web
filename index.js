2a83b7b1d86d9d3cb822c59db18a1d1ce36d815d
Wilizilla <wilian.ventura@gmail.com>
2025-11-21 01:10:06 -0300

    nodemon

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const express = require ('express'); // importação de módulo da pasta node-modules
 const app = express(); // por convenção o nome é app, mas poderia ser outro nome
-const port = 3303; // a porta normal seria 3000
+const porta = 3303; // a porta normal seria 3000
 
 
 // função para o servidor escutar alguma porta
-app.listen (3303, (error) => {
+app.listen (porta, (error) => {
     if(error) {
         console.log("Erro: servidor offline")
         return;
