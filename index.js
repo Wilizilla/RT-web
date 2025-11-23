f8eabb633e92f2d0aa82aa3bb4ae9827adf7b9b7
Wilizilla <wilian.ventura@gmail.com>
2025-11-23 04:17:11 -0300

    ajuste na prganização das pastas

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const app = express(); // por convenção o nome é app, mas poderia ser outro n
 const porta = 3303; // a porta normal seria 3000
 const routes = require ('./routes.js') // puxa as rotas
 const path = require ('path'); // modulo para manipular caminhos de arquivos e diretórios em diferentes SOs
-const globalMiddleware = require ('./src/middlewares/globalMiddleware')
+const globalMiddleware = require ('./src/middlewares/globalMiddleware.js') // configuração do middleware global
 
 app.use(express.urlencoded({extended: true})); // midware do express - Permite que o servidor leia dados de formulários que chegam por POST
 app.set('views', path.resolve(__dirname, 'src','views')); //camiho das views usando path
 app.set('view engine', 'ejs'); // configura uma engine para renderizar as views htmls 
 app.use(express.static(path.resolve(__dirname, 'public'))); // configura o acesso a o css/ imagens e outros arquivos necessários para montar a página
-app.use(globalMiddleware); // dessamaneira (app.use) todas as rotas usam esse middleware
-
+app.use(globalMiddleware); // middleware aplicado deta forma acaba sendo usado em todas rotas.
 app.use(routes); 
 
 // função para o servidor escutar alguma porta
--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,5 +1,4 @@
 // função para get na raiz do site (requisição e resposta)
-exports.productSearch = (req, res, next) => {
+exports.productSearch = (req, res) => {
     res.render('index');  // aqui vai o nome do template que deve ser renderizado
-    next;
 };
