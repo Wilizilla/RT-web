12710f6f3596e9902d0f3a93b685a4bf267243f5
Wilizilla <wilian.ventura@gmail.com>
2025-11-24 11:05:10 -0300

    ajustes de nomenclatura no arquivo routes.js - VERSÃO estavel com view basica sem interação

--- src/controllers/TesteController.js
+++ src/controllers/TesteController.js
@@ -1,4 +1,4 @@
 // função para get na raiz do site (requisição e resposta)
-exports.productSearch = (req, res) => {
+exports.indexTest = (req, res) => {
     res.render('index');  // aqui vai o nome do template que deve ser renderizado
 };
--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,9 +1,11 @@
 const express = require ('express');
 const route = express.Router(); // configuração inicial 
-const dataController = require ('../controllers/dataController'); // não precisaria identificar a extensão do arquivo
+const TesteController = require ('../controllers/TesteController'); // não precisaria identificar a extensão do arquivo
+
+
 
 //rotas banco de dados
-route.get ('/' , dataController.productSearch); // rota para listar os produtos
+route.get ('/' , TesteController.indexTest); // rota para listar os produtos
 //route.update ('/' , dataController.productUpdate);
 
 module.exports = route;
